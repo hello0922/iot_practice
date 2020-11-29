15ab7e54ca873cfc649f04a30e7f3aa6144e2bac
Mingyu Park <hello0922@.cau.ac.kr>
2020-11-29 13:03:29 +0900

    run example 1-6 (creating array as a member of obejct)

---
+++ hw1/1/1-6.js
@@ -0,0 +1,10 @@
+var obj = {
+    x: 3,
+    arr: [3, 4, 5],
+};
+
+console.log('obj = ', obj);
+console.log('obj.x = ', obj.x);
+console.log('obj.arr[0] = ', obj.arr[0]);
+console.log('obj.arr[1] = ', obj.arr[1]);
+console.log('obj.arr[2] = ', obj.arr[2]);
