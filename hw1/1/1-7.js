4438225d0b528f09b04945045be8cd2e82bb7120
Mingyu Park <hello0922@.cau.ac.kr>
2020-11-29 13:06:48 +0900

    run example 1-7 (copying values for value and object)

---
+++ hw1/1/1-7.js
@@ -0,0 +1,11 @@
+var a = 10;
+console.log('a = ' , a);
+var b = a;
+console.log('b = ' , b);
+
+var obj1 = { c: 10, d: 'ddd' };
+console.log('obj1.c = ' , obj1.c);
+console.log('obj1.d = ' , obj1.d);
+var obj2 = obj1;
+console.log('obj2.c = ' , obj2.c);
+console.log('obj2.d = ' , obj2.d);
