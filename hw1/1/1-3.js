7f5d3ae72d4921fde3ee843687998912bc749436
Mingyu Park <hello0922@.cau.ac.kr>
2020-11-29 12:57:50 +0900

    run example 1-3

---
+++ hw1/1/1-3.js
@@ -0,0 +1,11 @@
+var a = 'abc';
+console.log('var a = ', a);
+a = a + 'def';
+console.log('var a = ', a);
+
+var b = 5;
+console.log('var b = ', b);
+var c = 5;
+console.log('var c = ', c);
+b = 7;
+console.log('var b = ', b);
